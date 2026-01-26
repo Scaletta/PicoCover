62344db48d04dbfcbf2a6faa08f0e0491ee9c7eb
Jorgos Nomikos <jorgos.nomikos@dialoggroup.eu>
2026-01-26 22:07:04 +0100

    Update application name in resource configuration

--- build.rs
+++ build.rs
@@ -6,7 +6,7 @@ fn main() {
     if cfg!(target_os = "windows") {
     let mut res = winres::WindowsResource::new();
     res.set_icon("assets/icon.ico")
-       .set("Pico Launcher Cover Tool", "pico_launcher_cover_tool.exe")
+       .set("Pico Cover", "pico-cover.exe")
        // manually set version 1.0.0.0
        .set_version_info(winres::VersionInfo::PRODUCTVERSION, 0x0001000000000000);
     res.compile().unwrap();
